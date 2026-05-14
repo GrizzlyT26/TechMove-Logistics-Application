4c0829251c6e13d1ddb43172c3a95118d8b01ce7
USER-PC <kmafuna@bidtravel.co.za>
2026-05-14 14:32:43 +0200

    Added migration SQL script

---
+++ migration.sql
@@ -0,0 +1,66 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+CREATE TABLE [Clients] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NOT NULL,
+    [ContactDetails] nvarchar(max) NOT NULL,
+    [Region] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_Clients] PRIMARY KEY ([Id])
+);
+
+CREATE TABLE [Contracts] (
+    [Id] int NOT NULL IDENTITY,
+    [ClientId] int NOT NULL,
+    [StartDate] datetime2 NOT NULL,
+    [EndDate] datetime2 NOT NULL,
+    [Status] nvarchar(max) NOT NULL,
+    [ServiceLevel] nvarchar(max) NOT NULL,
+    [AgreementFilePath] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_Contracts] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Contracts_Clients_ClientId] FOREIGN KEY ([ClientId]) REFERENCES [Clients] ([Id]) ON DELETE CASCADE
+);
+
+CREATE TABLE [ServiceRequests] (
+    [Id] int NOT NULL IDENTITY,
+    [ContractId] int NOT NULL,
+    [Description] nvarchar(max) NOT NULL,
+    [CostUSD] decimal(18,2) NOT NULL,
+    [CostZAR] decimal(18,2) NOT NULL,
+    [Status] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_ServiceRequests] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_ServiceRequests_Contracts_ContractId] FOREIGN KEY ([ContractId]) REFERENCES [Contracts] ([Id]) ON DELETE CASCADE
+);
+
+CREATE INDEX [IX_Contracts_ClientId] ON [Contracts] ([ClientId]);
+
+CREATE INDEX [IX_ServiceRequests_ContractId] ON [ServiceRequests] ([ContractId]);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20260509121354_InitialCreate', N'9.0.0');
+
+DECLARE @var0 sysname;
+SELECT @var0 = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Contracts]') AND [c].[name] = N'AgreementFilePath');
+IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [Contracts] DROP CONSTRAINT [' + @var0 + '];');
+ALTER TABLE [Contracts] ALTER COLUMN [AgreementFilePath] nvarchar(max) NULL;
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20260509133820_FixAgreementNullable', N'9.0.0');
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20260509133909_UpdateContractAgreementPath', N'9.0.0');
+
+COMMIT;
+GO
+
